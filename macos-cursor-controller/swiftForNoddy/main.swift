bc6907da9b6d04331ee05ced39f3eb5fb9b057c9
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-05-27 23:05:51 +0900

    feat: AirPods API를 통하여 모션 데이터 가져오기 #4

--- macos-cursor-controller/swiftForNoddy/main.swift
+++ macos-cursor-controller/swiftForNoddy/main.swift
@@ -1,4 +1,31 @@
 import Foundation
+import CoreMotion
 
-print("Hello, World!")
+let motionManager = CMHeadphoneMotionManager()
 
+if !motionManager.isDeviceMotionAvailable {
+  print("에어팟 연결 안됨")
+  exit(1)
+} else {
+    var lastPitch: Double = 0
+    var lastYaw: Double = 0
+    let sensitivity: Double = 0.01
+
+    motionManager.startDeviceMotionUpdates(to: .main) { motion, error in
+      guard let motion = motion else { return }
+
+      let attitude = motion.attitude
+      let pitch = attitude.pitch
+      let yaw = -attitude.yaw
+
+      if abs(pitch - lastPitch) > sensitivity || abs(yaw - lastYaw) > sensitivity {
+
+        print(String(format: "pitch: %.2f, yaw: %.2f", pitch, yaw))
+
+        lastPitch = pitch
+        lastYaw = yaw
+      }
+    }
+  }
+
+RunLoop.main.run()
