d489f30b25a5bc505cf32fefef915d64e044b9c0
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-05-28 22:31:37 +0900

    fix: 좌클릭 기능 내 더블클릭 기능 추가 #4

--- macos-cursor-controller/swiftForNoddy/mouseEventHelper.swift
+++ macos-cursor-controller/swiftForNoddy/mouseEventHelper.swift
@@ -2,21 +2,35 @@ import AppKit
 import Quartz
 
 struct mouseEventHelper {
+    private static var lastClickTime: TimeInterval = 0
+    private static let doubleClickThreshold: TimeInterval = 0.3
+
     static func leftClickAtCurrentCursor() {
-        click(mouseTypeDown: .leftMouseDown, mouseTypeUp: .leftMouseUp, button: .left)
-    }
+        let currentTime: TimeInterval = Date().timeIntervalSince1970
+        let timeSinceLastClick: TimeInterval = currentTime - lastClickTime
 
+        if timeSinceLastClick < doubleClickThreshold {
+            click(mouseTypeDown: .leftMouseDown, mouseTypeUp: .leftMouseUp, button: .left, clickCount: 2)
+        } else {
+            click(mouseTypeDown: .leftMouseDown, mouseTypeUp: .leftMouseUp, button: .left, clickCount: 1)
+        }
+
+        lastClickTime = currentTime
+    }
     static func rightClickAtCurrentCursor() {
-        click(mouseTypeDown: .rightMouseDown, mouseTypeUp: .rightMouseUp, button: .right)
+        click(mouseTypeDown: .rightMouseDown, mouseTypeUp: .rightMouseUp, button: .right, clickCount: 1)
     }
 
-    private static func click(mouseTypeDown: CGEventType, mouseTypeUp: CGEventType, button: CGMouseButton) {
+    private static func click(mouseTypeDown: CGEventType, mouseTypeUp: CGEventType, button: CGMouseButton, clickCount: Int) {
         let screenHeight: CGFloat = NSScreen.screens.first?.frame.height ?? 0
         let location: NSPoint = NSEvent.mouseLocation
         let flippedLocation: CGPoint = CGPoint(x: location.x, y: screenHeight - location.y)
 
         let mouseDown: CGEvent? = CGEvent(mouseEventSource: nil, mouseType: mouseTypeDown, mouseCursorPosition: flippedLocation, mouseButton: button)
         let mouseUp: CGEvent? = CGEvent(mouseEventSource: nil, mouseType: mouseTypeUp, mouseCursorPosition: flippedLocation, mouseButton: button)
+
+        mouseDown?.setIntegerValueField(.mouseEventClickState, value: Int64(clickCount))
+        mouseUp?.setIntegerValueField(.mouseEventClickState, value: Int64(clickCount))
 
         mouseDown?.post(tap: .cghidEventTap)
         mouseUp?.post(tap: .cghidEventTap)
