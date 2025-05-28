800dc28dd49871782e557a225bf62c79535997db
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-05-28 14:38:52 +0900

    feat: 커서 이동 방식 변경(NSScreen 활용) #4

--- macos-cursor-controller/swiftForNoddy/main.swift
+++ macos-cursor-controller/swiftForNoddy/main.swift
@@ -1,31 +1,32 @@
 import Foundation
 import CoreMotion
 import CoreGraphics
+import AppKit
 
-let motionManager = CMHeadphoneMotionManager()
+let motionManager: CMHeadphoneMotionManager = CMHeadphoneMotionManager()
 
-var isReferenceSet = false
+var isReferenceSet: Bool = false
 var referencePitch: Double = 0
 var referenceYaw: Double = 0
 
 var lastPitch: Double = 0
 var lastYaw: Double = 0
 
-let sensitivity: Double = 0.01
+let sensitivity: Double = 0.005
 
-let pitchScale: CGFloat = 100.0
-let yawScale: CGFloat = 100.0
+let pitchScale: CGFloat = 3000.0
+let yawScale: CGFloat = 3000.0
 
 if !motionManager.isDeviceMotionAvailable {
     print("에어팟 연결 안됨")
     exit(1)
 } else {
     motionManager.startDeviceMotionUpdates(to: .main) { motion, error in
-        guard let motion = motion else { return }
+        guard let motion: CMDeviceMotion = motion else { return }
 
-        let attitude = motion.attitude
-        let pitch = attitude.pitch
-        let yaw = -attitude.yaw
+        let attitude: CMAttitude = motion.attitude
+        let pitch: Double = attitude.pitch
+        let yaw: Double = -attitude.yaw
 
         if !isReferenceSet {
             referencePitch = pitch
@@ -35,22 +36,22 @@ if !motionManager.isDeviceMotionAvailable {
             return
         }
 
-        let adjustedPitch = pitch - referencePitch
-        let adjustedYaw = yaw - referenceYaw
+        let adjustedPitch: Double = pitch - referencePitch
+        let adjustedYaw: Double = yaw - referenceYaw
 
         if abs(adjustedPitch - lastPitch) > sensitivity || abs(adjustedYaw - lastYaw) > sensitivity {
             print(String(format: "🎯 adjustedPitch: %.2f, adjustedYaw: %.2f", adjustedPitch, adjustedYaw))
             lastPitch = adjustedPitch
             lastYaw = adjustedYaw
 
-            if let loc = CGEvent(source: nil)?.location {
-                let dx = CGFloat(adjustedYaw) * yawScale
-                let dy = CGFloat(adjustedPitch) * pitchScale
+            if let screen: NSScreen = NSScreen.main {
+                let screenFrame: NSRect = screen.frame
+                let screenCenter: CGPoint = CGPoint(x: screenFrame.midX, y: screenFrame.midY)
 
-                let newX = loc.x + dx
-                let newY = loc.y - dy
+                let newX: CGFloat = screenCenter.x + CGFloat(adjustedYaw) * yawScale
+                let newY: CGFloat = screenCenter.y - CGFloat(adjustedPitch) * pitchScale
 
-                let move = CGEvent(
+                let move: CGEvent? = CGEvent(
                     mouseEventSource: nil,
                     mouseType: .mouseMoved,
                     mouseCursorPosition: CGPoint(x: newX, y: newY),
