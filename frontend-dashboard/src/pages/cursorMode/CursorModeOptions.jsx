42de7bcdd1a73eb7c17c1013f91277876c990c78
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-03 22:19:40 +0900

    feat: 공통 옵션 컴포넌트 생성 #25
    * 각 옵션 페이지에서 사용가능한 옵션 컴퍼넌트 생성
    * 옵션 컴퍼넌트 종류: 버튼 할당, 슬라이더

--- frontend-dashboard/src/pages/cursorMode/CursorModeOptions.jsx
+++ frontend-dashboard/src/pages/cursorMode/CursorModeOptions.jsx
@@ -1,7 +1,15 @@
+import ControlKey from "@/components/ControlKey";
+import ControlSlider from "@/components/ControlSlider";
+
 export default function CursorModeOptions() {
   return (
     <>
-      <div className="m-2 flex h-[50vh] w-[50vh] justify-center rounded-2xl border-2">CursorMode</div>
+      <div className="m-2 flex h-[50vh] w-[50vh] flex-col items-center justify-start rounded-2xl border-2">
+        <div className="font-bold">Option</div>
+        <ControlSlider name={"Cursor Sensitivity"} />
+        <ControlKey name={"Toggle Mode"} />
+        <ControlKey name={"Left Click"} />
+      </div>
     </>
   );
 }
