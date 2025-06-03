817be509a40473eb07521519a3c1c5ae98ba695c
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-03 22:17:29 +0900

    design: 정적 UI 스타일 수정 #25

--- frontend-dashboard/src/components/Footer.jsx
+++ frontend-dashboard/src/components/Footer.jsx
@@ -3,13 +3,13 @@ import HeadImage from "@/assets/headImage.jpg";
 export default function Footer() {
   return (
     <>
-      <div className="flex h-[20vh] w-[50vh] justify-center rounded-2xl border-2">
-        <img className="h-[19vh] w-[19vh] rounded-2xl" src={HeadImage}></img>
-        <div className="h-[19vh] w-[30vh] rounded-2xl border-2">
+      <div className="flex h-[20vh] w-[50vh] items-center justify-center rounded-2xl border-2">
+        <img className="mr-[2vh] h-[17vh] w-[17vh] rounded-2xl" src={HeadImage}></img>
+        <div className="flex h-[17vh] w-[25vh] flex-col items-center justify-center rounded-2xl border-2">
           <div className="flex justify-center">AirPods</div>
-          <div className="rounded-xl border-2">
-            <div>Name: Name</div>
-            <div>Battery: 90%</div>
+          <div className="mt-[1vh] flex h-[10vh] w-[20vh] flex-col justify-center rounded-xl border-2">
+            <div className="pl-2">Name: Name</div>
+            <div className="pl-2">Battery: 90%</div>
           </div>
         </div>
       </div>
--- frontend-dashboard/src/components/Header.jsx
+++ frontend-dashboard/src/components/Header.jsx
@@ -3,10 +3,10 @@ export default function Header() {
     <>
       <div className="h-[10vh] w-[50vh] rounded-2xl border-2">
         <div className="flex justify-center text-4xl font-extrabold">Noddy</div>
-        <div className="flex h-[4vh] w-[50vh]">
-          <button className="ml-auto h-[4vh] w-[5vh] cursor-pointer rounded-xl border-2">Cursor</button>
-          <button className="h-[4vh] w-[5vh] cursor-pointer rounded-xl border-2">Scroll</button>
-          <button className="h-[4vh] w-[5vh] cursor-pointer rounded-xl border-2">Keys</button>
+        <div className="mt-[1.5vh] flex h-[3vh] w-[50vh]">
+          <button className="ml-auto w-[7vh] cursor-pointer rounded-xl border-2">Cursor</button>
+          <button className="w-[7vh] cursor-pointer rounded-xl border-2">Scroll</button>
+          <button className="w-[7vh] cursor-pointer rounded-xl border-2">Keys</button>
         </div>
       </div>
     </>
