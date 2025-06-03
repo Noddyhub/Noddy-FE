60a66d41cd1198716cf1b9be35d146308d9329b8
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-03 21:20:02 +0900

    feat: 대시보드 정적UI 구현 #25

--- frontend-dashboard/src/components/Footer.jsx
+++ frontend-dashboard/src/components/Footer.jsx
@@ -1,3 +1,18 @@
+import HeadImage from "@/assets/headImage.jpg";
+
 export default function Footer() {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-[20vh] w-[50vh] justify-center rounded-2xl border-2">
+        <img className="h-[19vh] w-[19vh] rounded-2xl" src={HeadImage}></img>
+        <div className="h-[19vh] w-[30vh] rounded-2xl border-2">
+          <div className="flex justify-center">AirPods</div>
+          <div className="rounded-xl border-2">
+            <div>Name: Name</div>
+            <div>Battery: 90%</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend-dashboard/src/components/Header.jsx
+++ frontend-dashboard/src/components/Header.jsx
@@ -1,3 +1,14 @@
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <div className="h-[10vh] w-[50vh] rounded-2xl border-2">
+        <div className="flex justify-center text-4xl font-extrabold">Noddy</div>
+        <div className="flex h-[4vh] w-[50vh]">
+          <button className="ml-auto h-[4vh] w-[5vh] cursor-pointer rounded-xl border-2">Cursor</button>
+          <button className="h-[4vh] w-[5vh] cursor-pointer rounded-xl border-2">Scroll</button>
+          <button className="h-[4vh] w-[5vh] cursor-pointer rounded-xl border-2">Keys</button>
+        </div>
+      </div>
+    </>
+  );
 }
