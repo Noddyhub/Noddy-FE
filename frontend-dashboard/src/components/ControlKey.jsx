4c513e391f908553b28536756dc52b7d94e711fd
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-03 22:19:40 +0900

    feat: 공통 옵션 컴포넌트 생성 #25
    * 각 옵션 페이지에서 사용가능한 옵션 컴퍼넌트 생성
    * 옵션 컴퍼넌트 종류: 버튼 할당, 슬라이더

---
+++ frontend-dashboard/src/components/ControlKey.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function ControlKey({ name }) {
+  const [hotKey, setHotKey] = useState("");
+
+  return (
+    <div className="mt-2 flex h-[5vh] w-[48vh] flex-row items-center justify-between rounded-2xl border-2 px-2">
+      <div>{name}</div>
+      <div className="flex flex-row">
+        <input
+          className="rounded-2xl border-2 px-2"
+          placeholder="단축키.."
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              setHotKey(e.target.value);
+            }
+          }}
+        />
+        <div className="rounded-2xl border-2 pr-2 pl-2">{hotKey}</div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend-dashboard/src/components/ControlSlider.jsx
@@ -0,0 +1,10 @@
+export default function ControlSlider({ name }) {
+  return (
+    <div className="mt-2 flex h-[5vh] w-[48vh] flex-row items-center justify-between rounded-2xl border-2 px-2">
+      <div>{name}</div>
+      <div className="flex flex-row">
+        <input type="range" className="rounded-2xl border-2 px-2" />
+      </div>
+    </div>
+  );
+}
