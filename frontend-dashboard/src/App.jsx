a8d54c4044ad0553a90ae283c68b43e4526fe5e7
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-04 16:50:08 +0900

    feat: 라우팅 기능을 이용한 페이지 전환 구현 #31

--- frontend-dashboard/src/App.jsx
+++ frontend-dashboard/src/App.jsx
@@ -1,10 +1,14 @@
-import MainLayout from "@/layouts/MainLayout";
-import CursorModeOptions from "@/pages/cursorMode/cursorModeOptions";
+import { Routes, Route } from "react-router-dom";
+import InstructionPageLayout from "@/layouts/InstructionPageLayout";
+import OptionPageLayout from "@/layouts/OptionPageLayout";
+import StartPageLayout from "@/layouts/StartPageLayout";
 
 export default function App() {
   return (
-    <MainLayout>
-      <CursorModeOptions />
-    </MainLayout>
+    <Routes>
+      <Route path="/" element={<StartPageLayout />} />
+      <Route path="/instruction-page" element={<InstructionPageLayout />} />
+      <Route path="/option-page" element={<OptionPageLayout />} />
+    </Routes>
   );
 }
