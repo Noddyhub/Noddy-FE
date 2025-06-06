6f5211657c59c97794d5fc2964dd9eb520cc78b5
Yun0825 <guddbs5545@naver.com>
2025-06-06 14:21:23 +0900

    feat: socket 통신을 통한 pitch, yaw 데이터 수신 #33

--- frontend-dashboard/src/stores/useMovementStore.js
+++ frontend-dashboard/src/stores/useMovementStore.js
@@ -2,6 +2,10 @@ import { create } from "zustand";
 
 export const useMovementStore = create((set) => ({
   optionPage: "Cursor",
+  pitch: 0,
+  yaw: 0,
 
   setOptionPage: (optionPage) => set({ optionPage }),
+  setPitch: (pitch) => set({ pitch }),
+  setYaw: (yaw) => set({ yaw })
 }));
