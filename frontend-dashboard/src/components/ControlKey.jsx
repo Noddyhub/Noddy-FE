fa5f5b85700756cd34b504fc5d5094ea753d9a38
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-08 20:38:11 +0900

    design: 세부 스타일 수정 #43

--- frontend-dashboard/src/components/ControlKey.jsx
+++ frontend-dashboard/src/components/ControlKey.jsx
@@ -4,7 +4,7 @@ export default function ControlKey({ name }) {
   return (
     <div className="mb-2 flex w-[45vh] flex-row items-center justify-between rounded-2xl px-2">
       <div className="mt-2 mb-2 ml-2 font-medium dark:text-white">{name}</div>
-      <div className="flex flex-row">
+      <div>
         <OptionsList name={name} />
       </div>
     </div>
--- frontend-dashboard/src/components/Footer.jsx
+++ frontend-dashboard/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import ThreeDimensionalImage from "@/components/ThreeDimensionalImage";
 
 export default function Footer() {
   return (
-    <div className="mt-[2vh] flex items-center justify-center rounded-2xl">
-      <div className="mr-[3vh] h-[25vh] w-[25vh] overflow-hidden rounded-2xl">
+    <div className="mt-[2vh] flex w-[45vh] items-center justify-between rounded-2xl px-2">
+      <div className="h-[25vh] w-[25vh] overflow-hidden rounded-2xl">
         <ThreeDimensionalImage />
       </div>
       <div className="flex h-[25vh] w-[14vh] flex-col items-center justify-center rounded-2xl bg-gray-200 dark:bg-gray-800 dark:text-white">
