76a85ec0c708d52160474664e08c48f1f2859c6d
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-08 20:09:10 +0900

    feat: 3D 이미지 크기 조절 #43

--- frontend-dashboard/src/components/Footer.jsx
+++ frontend-dashboard/src/components/Footer.jsx
@@ -2,11 +2,11 @@ import ThreeDimensionalImage from "@/components/ThreeDimensionalImage";
 
 export default function Footer() {
   return (
-    <div className="flex h-[20vh] w-[50vh] items-center justify-center rounded-2xl">
-      <div className="mr-[2vh] h-[17vh] w-[17vh] overflow-hidden rounded-2xl">
+    <div className="mt-[2vh] flex items-center justify-center rounded-2xl">
+      <div className="mr-[3vh] h-[25vh] w-[25vh] overflow-hidden rounded-2xl">
         <ThreeDimensionalImage />
       </div>
-      <div className="flex h-[17vh] w-[25vh] flex-col items-center justify-center rounded-2xl bg-gray-200 dark:bg-gray-800 dark:text-white">
+      <div className="flex h-[25vh] w-[14vh] flex-col items-center justify-center rounded-2xl bg-gray-200 dark:bg-gray-800 dark:text-white">
         <div className="flex justify-center">AirPods</div>
         <div className="pl-2">Name: Name</div>
         <div className="pl-2">Battery: 90%</div>
