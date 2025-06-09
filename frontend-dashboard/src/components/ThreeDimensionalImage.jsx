efcfb6617e694a2d34eb0401180beaa6bfb4b0b5
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-09 17:18:24 +0900

    feat: 3D 컴포넌트가 받는 옵션 props 추가 #48

--- frontend-dashboard/src/components/ThreeDimensionalImage.jsx
+++ frontend-dashboard/src/components/ThreeDimensionalImage.jsx
@@ -3,16 +3,16 @@ import { useThemeStore } from "@/stores/useThemeStore";
 
 export default function ThreeDimensionalImage(options) {
   const isThemeDark = useThemeStore((state) => state.isThemeDark);
-  const { rounded, model } = options;
+  const { rounded, model, cameraRotation } = options;
 
   return (
     <Canvas
       dpr={window.devicePixelRatio}
-      camera={{ fov: 8, position: [0, 0, -30], near: 1, far: 1000 }}
+      camera={{ fov: 8, position: [0, cameraRotation || 0, -30], near: 1, far: 1000 }}
       style={{
         background: isThemeDark ? "white" : "black",
       }}
-      className={`rounded-${rounded}`}
+      className={`rounded-${rounded ? rounded : "2xl"}`}
     >
       <hemisphereLight args={["#ffffff", "#444444", 1.5]} position={[0, 20, 0]} />
       {model}
