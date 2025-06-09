a3e417cac342463f8b3b016594796a72f396cb20
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-09 16:06:32 +0900

    refactor: 불필요한 props 제거 #48

--- frontend-dashboard/src/components/Model3D.jsx
+++ frontend-dashboard/src/components/Model3D.jsx
@@ -4,28 +4,25 @@ import { useFrame } from "@react-three/fiber";
 import { useThemeStore } from "@/stores/useThemeStore";
 import { useMovementStore } from "@/stores/useMovementStore";
 
-export default function Model3D({ modelColor }) {
+export default function Model3D({ direction }) {
   const { scene } = useGLTF("/headImage.gltf");
   const modelRef = useRef();
   const isThemeDark = useThemeStore((state) => state.isThemeDark);
   const { pitch, yaw } = useMovementStore();
+  const rotatingDirection = direction === 3.8 ? 1 : -1;
 
   useEffect(() => {
     scene.traverse((child) => {
-      if (modelColor && child.isMesh) {
-        child.material.color.set(modelColor);
-      } else {
-        if (child.isMesh) {
-          child.material.color.set(isThemeDark ? "rgb(140,140,140)" : "white");
-        }
+      if (child.isMesh) {
+        child.material.color.set(isThemeDark ? "rgb(140,140,140)" : "white");
       }
     });
-  }, [isThemeDark, scene, modelColor]);
+  }, [isThemeDark, scene]);
 
   useFrame(() => {
     if (modelRef.current) {
       modelRef.current.rotation.x = (pitch - 0.5) * window.innerWidth * 0.0004;
-      modelRef.current.rotation.y = (yaw - 3.8) * window.innerHeight * 0.001;
+      modelRef.current.rotation.y = rotatingDirection * (yaw - direction) * window.innerHeight * 0.001;
     }
   });
 
--- frontend-dashboard/src/components/ThreeDimensionalImage.jsx
+++ frontend-dashboard/src/components/ThreeDimensionalImage.jsx
@@ -1,22 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { useThemeStore } from "@/stores/useThemeStore";
-import Model3D from "@/components/Model3D";
 
 export default function ThreeDimensionalImage(options) {
   const isThemeDark = useThemeStore((state) => state.isThemeDark);
-  const { rounded, modelColor, backgroundColor } = options;
+  const { rounded, model } = options;
 
   return (
     <Canvas
       dpr={window.devicePixelRatio}
       camera={{ fov: 8, position: [0, 0, -30], near: 1, far: 1000 }}
       style={{
-        background: backgroundColor || (isThemeDark ? "white" : "black"),
+        background: isThemeDark ? "white" : "black",
       }}
       className={`rounded-${rounded}`}
     >
       <hemisphereLight args={["#ffffff", "#444444", 1.5]} position={[0, 20, 0]} />
-      <Model3D modelColor={modelColor} />
+      {model}
     </Canvas>
   );
 }
