25827207c80352c426a10198f7f20c8e036faf27
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-10 16:47:15 +0900

    refactor: 테마 토글 버튼 컴퍼넌트화 #48

--- frontend-dashboard/src/App.jsx
+++ frontend-dashboard/src/App.jsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import { useThemeStore } from "@/stores/useThemeStore";
-import { MoonIcon, SunIcon } from "@heroicons/react/20/solid";
 import InstructionPageLayout from "@/layouts/InstructionPageLayout";
 import OptionPageLayout from "@/layouts/OptionPageLayout";
 import StartPageLayout from "@/layouts/StartPageLayout";
+import ThemeToggleButton from "@/components/ThemeToggleButton";
 import DetailedInstructionPageLayout from "@/layouts/DetailedInstructionPageLayout";
 import Model3D from "@/components/Model3D";
 import VirtualWindow from "@/components/VirtualWindow";
 import VirtualArrowKeys from "@/components/VirtualArrowKeys";
 
 export default function App() {
-  const { isThemeDark, setIsThemeDark } = useThemeStore();
   const VALUE_ROTATING_MODEL_BACKWARD = 0.5;
   const VALUE_MOVING_MODEL_DOWN = -1.6;
   const VALUE_MOVING_CAMERA_UP = 20;
@@ -18,14 +16,7 @@ export default function App() {
   return (
     <>
       <div className="absolute -z-100 h-[100vh] w-[50vh] bg-gray-50 dark:bg-gray-700"></div>
-      <div className="pointer-events-none absolute z-50 h-[100vh] w-[50vh]">
-        <button
-          className="pointer-events-auto absolute top-4 right-6 w-[6vh] cursor-pointer rounded-4xl bg-gray-200 px-4 py-2 shadow dark:bg-gray-800 dark:text-white"
-          onClick={setIsThemeDark}
-        >
-          {isThemeDark ? <SunIcon /> : <MoonIcon />}
-        </button>
-      </div>
+      <ThemeToggleButton />
       <Routes>
         <Route path="/" element={<StartPageLayout />} />
         <Route path="/instruction-page" element={<InstructionPageLayout />}>
