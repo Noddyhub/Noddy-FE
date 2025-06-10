e2fd4ba2b532da9b0abacb3b7a58b726ef181e7e
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-10 10:13:06 +0900

    feat: 슬라이더 옵션 값 표시 #48

--- frontend-dashboard/src/components/ControlSlider.jsx
+++ frontend-dashboard/src/components/ControlSlider.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import useSocket from "../hooks/useSocket";
 
 export default function ControlSlider(props) {
   const { name, min, max, step, defaultValue } = props;
   const { sendMessage, clientId } = useSocket();
+  const [value, setValue] = useState(defaultValue);
 
   const handleSliderChange = (e) => {
     if (!clientId) return;
     const sliderValue = Number(e.target.value);
+    setValue(e.target.value);
     sendMessage(JSON.stringify({ type: "control", name, value: sliderValue, clientId }));
   };
 
   return (
     <div className="mb-2 flex w-[45vh] flex-row items-center justify-between rounded-2xl px-2">
-      <div className="mt-2 mb-2 ml-2 font-medium dark:text-white">{name}</div>
+      <div className="mt-2 mb-2 ml-2 font-medium dark:text-white">
+        {name} : <span className="font-bold">{value}</span>
+      </div>
       <div className="flex flex-row">
         <input
           type="range"
