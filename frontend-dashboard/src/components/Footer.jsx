388a16bc8facf03d0854d82ff30788405d9a4c1f
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-10 20:51:17 +0900

    refactor: 에어팟 상태 데이터 컴퍼넌트화 #51

--- frontend-dashboard/src/components/Footer.jsx
+++ frontend-dashboard/src/components/Footer.jsx
@@ -1,9 +1,8 @@
-import { useDeviceInfoStore } from "@/stores/useDeviceInfoStore";
 import ThreeDimensionalImage from "@/components/ThreeDimensionalImage";
 import Model3D from "@/components/Model3D";
+import DeviceInfo from "@/components/DeviceInfo";
 
 export default function Footer() {
-  const { macBattery, airPodsName, airPodsLeftBattery, airPodsRightBattery } = useDeviceInfoStore();
   const VALUE_ROTATING_MODEL_FORWARD = 3.8;
 
   return (
@@ -11,12 +10,7 @@ export default function Footer() {
       <div className="h-[25vh] w-[25vh] overflow-hidden rounded-2xl">
         <ThreeDimensionalImage model={<Model3D direction={VALUE_ROTATING_MODEL_FORWARD} />} />
       </div>
-      <div className="flex h-[25vh] w-[17vh] flex-col items-center justify-center rounded-2xl bg-gray-200 text-sm dark:bg-gray-800 dark:text-white">
-        <div className="flex justify-center">Name : {airPodsName}</div>
-        <div className="flex justify-center">Mac Battery: {macBattery}%</div>
-        <div className="flex justify-center">Left: {airPodsLeftBattery}%</div>
-        <div className="flex justify-center">Right: {airPodsRightBattery}%</div>
-      </div>
+      <DeviceInfo />
     </div>
   );
 }
