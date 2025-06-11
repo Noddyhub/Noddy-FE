7037fb34229e8eaf2bdc0133d2aa00a561f9fbad
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-11 23:56:10 +0900

    feat: 앱 실행 시 상단에 도구막대 표시 기능 추가 #56

---
+++ macos-cursor-controller/swiftForNoddy/AppDelegate.swift
@@ -0,0 +1,39 @@
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+
+    let popover = NSPopover()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(togglePopover(_:))
+        }
+        popover.contentViewController = ToolBarViewController.freshController()
+
+        mainLogic()
+    }
+
+    @objc func togglePopover(_ sender: Any?) {
+      if popover.isShown {
+        closePopover(sender: sender)
+      } else {
+        showPopover(sender: sender)
+      }
+    }
+
+    func showPopover(sender: Any?) {
+      if let button = statusItem.button {
+          popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+      }
+    }
+
+    func closePopover(sender: Any?) {
+      popover.performClose(sender)
+    }
+}
+
+
