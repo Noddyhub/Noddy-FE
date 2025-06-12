a89e0c62bab947d4f585d40860600dec22f42ecb
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-12 22:22:03 +0900

    feat: 배터리 잔량 시각화 #59

--- frontend-dashboard/src/components/DeviceInfo.jsx
+++ frontend-dashboard/src/components/DeviceInfo.jsx
@@ -6,14 +6,28 @@ function AirPodsInfo() {
   return (
     <div className="mb-[1vh] flex h-[12vh] w-[17vh] flex-col items-center justify-center rounded-2xl bg-gray-200 text-sm dark:bg-gray-800 dark:text-white">
       <div className="flex h-[3vh] items-center text-xl font-bold">{airPodsName}</div>
-      <div className="flex h-[6vh] flex-col justify-center">
-        <div>
-          <span className="font-semibold">L 유닛 배터리: </span>
-          {airPodsLeftBattery}%
-        </div>
-        <div>
-          <span className="font-semibold">R 유닛 배터리: </span>
-          {airPodsRightBattery}%
+      <div className="flex h-[7.5vh] flex-col justify-center">
+        <div className="flex flex-col items-center">
+          <div className="mb-0.5 text-xs font-semibold">L 유닛 배터리</div>
+          <div className="mb-1.5 h-2 w-18 rounded bg-gray-300">
+            <div
+              className="h-full rounded"
+              style={{
+                width: `${airPodsLeftBattery}%`,
+                backgroundColor: airPodsLeftBattery <= 20 ? "red" : airPodsLeftBattery < 80 ? "gold" : "limegreen",
+              }}
+            />
+          </div>
+          <div className="mb-0.5 text-xs font-semibold">R 유닛 배터리</div>
+          <div className="h-2 w-18 rounded bg-gray-300">
+            <div
+              className="h-full rounded"
+              style={{
+                width: `${airPodsRightBattery}%`,
+                backgroundColor: airPodsRightBattery <= 20 ? "red" : airPodsRightBattery < 80 ? "gold" : "limegreen",
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -26,10 +40,18 @@ function MacInfo() {
   return (
     <div className="flex h-[12vh] w-[17vh] flex-col items-center justify-center rounded-2xl bg-gray-200 text-sm dark:bg-gray-800 dark:text-white">
       <div className="flex h-[3vh] items-center text-xl font-bold">Mac</div>
-      <div className="flex h-[6vh] flex-col justify-center">
-        <div>
-          <span className="font-semibold">본체 배터리: </span>
-          {macBattery}%
+      <div className="flex h-[7.5vh] flex-col justify-center">
+        <div className="flex flex-col items-center">
+          <div className="mb-0.5 font-semibold">본체 배터리</div>
+          <div className="h-2 w-18 rounded bg-gray-300">
+            <div
+              className="h-full rounded"
+              style={{
+                width: `${macBattery}%`,
+                backgroundColor: macBattery <= 20 ? "red" : macBattery < 80 ? "gold" : "limegreen",
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
