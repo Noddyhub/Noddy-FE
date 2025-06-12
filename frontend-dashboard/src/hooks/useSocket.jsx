49d40a1e00f0a33471ebbca07a1e818977fcb37e
Yun0825 <guddbs5545@naver.com>
2025-06-12 19:56:05 +0900

    test: motion 데이터 수신 테스트 #55

--- frontend-dashboard/src/hooks/useSocket.jsx
+++ frontend-dashboard/src/hooks/useSocket.jsx
@@ -25,6 +25,12 @@ export default function useSocket() {
       };
 
       socket.onmessage = (event) => {
+        console.log("pitch 데이터 수신됨: ", event.data.pitch);
+        console.log("yaw 데이터 수신됨: ", event.data.yaw);
+        console.log("macBaattery 데이터 수신됨: ", event.data.macBattery);
+        console.log("name 데이터 수신됨: ", event.data.name);
+        console.log("airpodLeftBattery 데이터 수신됨: ", event.data.airpodLeftBattery);
+        console.log("airpodRightBattery 데이터 수신됨: ", event.data.airpodRightBattery);
         try {
           const message = JSON.parse(event.data);
           setPitch(message.pitch);
