55386964fec14303cc7020fbb59cc8f894fcbd66
Yun0825 <guddbs5545@naver.com>
2025-06-13 20:02:44 +0900

    chore: optionPage를 키값으로 활용하기 위한 값 변경 #61

--- frontend-dashboard/src/stores/usePageStore.js
+++ frontend-dashboard/src/stores/usePageStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const usePageStore = create((set) => ({
-  optionPage: "커서",
+  optionPage: "cursor",
 
   setOptionPage: (optionPage) => set({ optionPage }),
 }));
