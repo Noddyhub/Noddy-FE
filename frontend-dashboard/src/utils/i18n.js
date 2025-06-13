000b2c855ff6d9e8d8ae00637a5f2499ffa71ee8
Yun0825 <guddbs5545@naver.com>
2025-06-13 19:51:01 +0900

    feat: 한국어, 영어 지원을 위한 util 파일 생성 #61

---
+++ frontend-dashboard/src/utils/i18n.js
@@ -0,0 +1,79 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+const resources = {
+  en: {
+    translation: {
+      start: "START",
+      cursorGuide: "Please refer to the image above and look at the front",
+      directionKeyGuide: "Press the arrow keys to fine-tune the cursor",
+      prev: "Prev",
+      next: "Next",
+      skip: "Skip",
+      cursor: "Cursor",
+      cursorSensitivity: "Cursor Sensitivity",
+      cursorReactionSpeed: "Cursor Reaction Speed",
+      toggleMode: "Toggle Mode",
+      leftClick: "Left Click",
+      rightClick: "Right Click",
+      cursorPause: "Pause Cursor",
+      cursorUp: "Move Cursor Up",
+      cursorDown: "Move Cursor Down",
+      cursorLeft: "Move Cursor Left",
+      cursorRight: "Move Cursor Right",
+      shortcutSelection: "shortcut",
+      scroll: "Scroll",
+      scrollSpeed: "Scroll Speed",
+      scrollPause: "Pause Scroll",
+      leftUnitBattery: "L unit battery",
+      rightUnitBattery: "R unit battery",
+      macBattery: "battery",
+    },
+  },
+  ko: {
+    translation: {
+      start: "시작",
+      cursorGuide: "위 이미지를 참고해 정면을 바라봐 주세요",
+      directionKeyGuide: "방향키를 눌러서 커서를 세부조정 해보세요",
+      prev: "이전",
+      next: "다음",
+      skip: "넘김",
+      cursor: "커서",
+      cursorSensitivity: "커서 민감도",
+      cursorReactionSpeed: "커서 반응속도",
+      toggleMode: "모드 변환",
+      leftClick: "왼쪽 클릭",
+      rightClick: "오른쪽 클릭",
+      cursorPause: "커서 일시정지",
+      cursorUp: "커서 위로 이동",
+      cursorDown: "커서 아래로 이동",
+      cursorLeft: "커서 왼쪽으로 이동",
+      cursorRight: "커서 오른쪽으로 이동",
+      shortcutSelection: "단축키 선택",
+      scroll: "스크롤",
+      scrollSpeed: "스크롤 속도",
+      scrollPause: "스크롤 일시정지",
+      leftUnitBattery: "L 유닛 베터리",
+      rightUnitBattery: "R 유닛 베터리",
+      macBattery: "본체 배터리",
+    },
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "ko",
+    detection: {
+      order: ["navigator"],
+      caches: [],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
