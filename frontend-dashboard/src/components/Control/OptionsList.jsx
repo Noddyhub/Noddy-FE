838c37b1667ead9b35eb7953a4bc9ed5e3ec723e
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-14 17:43:57 +0900

    feat: 옵션 버튼 클릭 시 드롭다운 표시 방식으로 변경 #63

--- frontend-dashboard/src/components/Control/OptionsList.jsx
+++ frontend-dashboard/src/components/Control/OptionsList.jsx
@@ -1,38 +1,33 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import useSocket from "@/hooks/useSocket";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { keyNameToKeyCode } from "@/constants/keyCodes";
 
 export default function OptionsList({ name }) {
   const [hotKey, setHotkey] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
   const { sendMessage, clientId } = useSocket();
   const { t } = useTranslation();
 
   const handleSelectChange = (e) => {
     const selectedHotKey = e.target.value;
-
     setHotkey(selectedHotKey);
     sendMessage(JSON.stringify({ type: "hotkey", name, value: selectedHotKey.charCodeAt(0), clientId }));
   };
 
   return (
-    <div className="relative w-32">
-      <select
-        className={`h-7 w-[13.5vh] appearance-none rounded-lg bg-gray-200 px-2 font-medium shadow dark:bg-gray-800 ${
-          hotKey === "" ? "text-gray-400 dark:text-gray-700" : "text-black dark:text-white"
-        }`}
-        value={hotKey}
-        onChange={handleSelectChange}
+    <div className="relative">
+      <button
+        onClick={() => setShowDropdown((prev) => !prev)}
+        className="w-[12.5vh] cursor-pointer rounded-lg bg-gray-200 py-0.5 shadow dark:bg-gray-800 dark:text-white"
       >
-        <option value="" disabled>
-          {t("shortcutSelection")}
-        </option>
-        {Object.entries(keyNameToKeyCode).map(([key, value]) => (
-          <option key={value}>{key}</option>
-        ))}
-      </select>
-      <ChevronDownIcon className="pointer-events-none absolute top-1/2 right-2 h-4 w-4 -translate-y-1/2 text-gray-500" />
+        {t("shortcutSelection")}
+      </button>
+      {showDropdown && (
+        <div className="absolute right-0 z-50 mt-2 flex h-[15vh] w-[30vh] items-center justify-center rounded-lg bg-gray-300 p-2 shadow dark:bg-gray-600">
+          키보드
+        </div>
+      )}
     </div>
   );
 }
