2e566dba8f893b4853e8b135bcf5e42865326cf2
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-14 23:19:50 +0900

    feat: 키 버튼 누름에 따른 키코드 출력 기능 추가 #63

--- frontend-dashboard/src/components/VirtualImage/VirtualKeyboard.jsx
+++ frontend-dashboard/src/components/VirtualImage/VirtualKeyboard.jsx
@@ -1,11 +1,19 @@
+import { keyNameToKeyCode } from "@/constants/keyCodes";
+
 export default function VirtualKeyboard() {
-  const KeyboardStlye = ({ keys }) => {
+  const handleClick = (value) => {
+    console.log(value);
+  };
+
+  const KeyboardStlye = ({ keys, values }) => {
     return (
       <div className="grid auto-cols-max grid-flow-col justify-center gap-1 text-black dark:text-white">
-        {keys.map((key) => (
+        {keys.map((key, index) => (
           <div
             key={key}
-            className="flex h-7 w-6 items-center justify-center rounded bg-white px-2 shadow-inner dark:bg-gray-700"
+            value={values[index]}
+            className="flex h-7 w-6 cursor-pointer items-center justify-center rounded bg-white px-2 shadow-inner hover:bg-gray-200 dark:bg-gray-700"
+            onClick={() => handleClick(values[index])}
           >
             {key}
           </div>
@@ -17,12 +25,27 @@ export default function VirtualKeyboard() {
   return (
     <div className="absolute right-0 z-100 mt-2 flex w-[40vh] items-center justify-center rounded-lg bg-gray-300 p-2 text-[1.2vh] font-semibold shadow dark:bg-gray-600">
       <div className="grid gap-1">
-        <KeyboardStlye keys={["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"]} />
-        <KeyboardStlye keys={[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]} />
-        <KeyboardStlye keys={["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]} />
-        <KeyboardStlye keys={["A", "S", "D", "F", "G", "H", "J", "K", "L"]} />
-        <KeyboardStlye keys={["Z", "X", "C", "V", "B", "N", "M"]} />
-        <KeyboardStlye keys={["TAB", "↑", "↓", "←", "→"]} />
+        <KeyboardStlye
+          keys={Object.keys(keyNameToKeyCode.functionKeys)}
+          values={Object.values(keyNameToKeyCode.functionKeys)}
+        />
+        <KeyboardStlye keys={Object.keys(keyNameToKeyCode.numbers)} values={Object.values(keyNameToKeyCode.numbers)} />
+        <KeyboardStlye
+          keys={Object.keys(keyNameToKeyCode.alphabetLine1)}
+          values={Object.values(keyNameToKeyCode.alphabetLine1)}
+        />
+        <KeyboardStlye
+          keys={Object.keys(keyNameToKeyCode.alphabetLine2)}
+          values={Object.values(keyNameToKeyCode.alphabetLine2)}
+        />
+        <KeyboardStlye
+          keys={Object.keys(keyNameToKeyCode.alphabetLine3)}
+          values={Object.values(keyNameToKeyCode.alphabetLine3)}
+        />
+        <KeyboardStlye
+          keys={Object.keys(keyNameToKeyCode.tabAndArrowKeys)}
+          values={Object.values(keyNameToKeyCode.tabAndArrowKeys)}
+        />
       </div>
     </div>
   );
