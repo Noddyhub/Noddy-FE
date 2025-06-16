1959fc06e05ed30a93f182350752ca790cd3a7e9
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-16 14:55:49 +0900

    feat: 앱 시작시 옵션 단축키 초기값 제공 #63

--- frontend-dashboard/src/stores/useHotkeyStore.js
+++ frontend-dashboard/src/stores/useHotkeyStore.js
@@ -1,7 +1,16 @@
 import { create } from "zustand";
 
 export const useHotkeyStore = create((set) => ({
-  assignedHotkeys: {},
+  assignedHotkeys: {
+    "모드 변환": "TAB",
+    좌클릭: "F9",
+    우클릭: "F10",
+    "커서 조작 일시정지": "F11",
+    "커서 위로": "↑",
+    "커서 아래로": "↓",
+    "커서 왼쪽으로": "←",
+    "커서 오른쪽으로": "→",
+  },
 
   setAssignedHotkeys: (newHotkey) =>
     set((state) => {
