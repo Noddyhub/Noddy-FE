784704d07bccbe4ae588bbc84438a7af333d00f1
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-16 22:36:44 +0900

    chore: 최신 develop 브랜치 기준으로 한국어 값 수정 #63

--- frontend-dashboard/src/stores/useHotkeyStore.js
+++ frontend-dashboard/src/stores/useHotkeyStore.js
@@ -3,13 +3,13 @@ import { create } from "zustand";
 export const useHotkeyStore = create((set) => ({
   assignedHotkeys: {
     "모드 변환": "TAB",
-    좌클릭: "F9",
-    우클릭: "F10",
-    "커서 조작 일시정지": "F11",
-    "커서 위로": "↑",
-    "커서 아래로": "↓",
-    "커서 왼쪽으로": "←",
-    "커서 오른쪽으로": "→",
+    "왼쪽 클릭": "F9",
+    "오른쪽 클릭": "F10",
+    "커서 일시정지": "F11",
+    "커서 위로 이동": "↑",
+    "커서 아래로 이동": "↓",
+    "커서 왼쪽으로 이동": "←",
+    "커서 오른쪽으로 이동": "→",
   },
 
   setAssignedHotkeys: (newHotkey) =>
