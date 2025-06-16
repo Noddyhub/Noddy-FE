02fe8f509aafbeee29b60231ea06003771e9df2c
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-16 14:55:49 +0900

    feat: 앱 시작시 옵션 단축키 초기값 제공 #63

--- frontend-dashboard/src/components/Control/ControlKey.jsx
+++ frontend-dashboard/src/components/Control/ControlKey.jsx
@@ -7,7 +7,7 @@ export default function ControlKey({ name }) {
   return (
     <div className="mb-2 flex w-[45vh] flex-row items-center justify-between rounded-2xl px-2">
       <div className="mt-2 mb-2 ml-2 text-lg font-semibold dark:text-white">
-        {name} : {assignedHotkeys[name]}
+        {name} : <span className="font-bold">{assignedHotkeys[name]}</span>
       </div>
       <div>
         <OptionsList name={name} />
