16fe247ca22a55ae7cefd51aada42a3c2ee263bc
Yun0825 <guddbs5545@naver.com>
2025-06-18 02:56:22 +0900

    feat: handleKeyValue Props 전달 #67

--- frontend-dashboard/src/components/VirtualImage/VirtualKeyboard.jsx
+++ frontend-dashboard/src/components/VirtualImage/VirtualKeyboard.jsx
@@ -1,7 +1,7 @@
 import { keyNameToKeyCode } from "@/constants/keyCodes";
 import KeyboardStyle from "@/components/VirtualImage/KeyboardStyle";
 
-export default function VirtualKeyboard({ keyName, name, defaultValue }) {
+export default function VirtualKeyboard({ keyName, name, defaultValue, handleButtonPress }) {
   return (
     <div className="absolute right-0 z-100 mt-2 flex w-[40vh] items-center justify-center rounded-lg bg-gray-300 p-2 text-[1.2vh] font-semibold shadow dark:bg-gray-600">
       <div className="grid gap-1">
@@ -13,6 +13,7 @@ export default function VirtualKeyboard({ keyName, name, defaultValue }) {
             name={name}
             keyName={keyName}
             defaultValue={defaultValue}
+            handleButtonPress={handleButtonPress}
           />
         ))}
       </div>
